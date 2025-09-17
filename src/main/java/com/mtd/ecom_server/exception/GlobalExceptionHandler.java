0d330affac9bb1215f4dbe5392a13729617f774a
Gireesh Patgar <gireeshpatgar17@gmail.com>
2025-09-17 14:24:17 +0530

    removed an import

--- src/main/java/com/mtd/ecom_server/exception/GlobalExceptionHandler.java
+++ src/main/java/com/mtd/ecom_server/exception/GlobalExceptionHandler.java
@@ -2,7 +2,6 @@ package com.mtd.ecom_server.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 //@ControllerAdvice
